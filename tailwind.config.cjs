df671d847c41285459bc19be0abe02e40963ce0e
Renan Veronez Drechsler <renanvz@outlook.com>
2022-10-12 19:38:28 +0100

    Pre-config tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,25 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
+
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-900': '#7c7c8a',
+      'gray-900': '#c4c4cc',
+      'gray-900': '#e1e1e6',
+
+      'cyan-500': '#81d8f7',
+      'cyan-300': '#9be1fb',
+    },
     extend: {
       fontFmaily: {
         sans: 'Inter, sans-serif'
