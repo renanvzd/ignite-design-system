32b0a56bd74c02afa47417b935fdef7940cee263
Renan Veronez Drechsler <renanvz@outlook.com>
2022-10-12 18:21:44 +0100

    Tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+  ],
+  theme: {
+    extend: {
+      fontFmaily: {
+        sans: 'Inter, sans-serif'
+      },
+    },
+  },
+  plugins: [],
+}
